721c03c222729055f40cc1dccce9692eb68169ea
Ezra <ezra.farrar@gmail.com>
2020-09-14 02:02:28 -0700

    Updates

---
+++ precache-manifest.abd791e6a3284fd62d725813394b91f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d60e41cb561e58b37c83300099f5a6",
+    "url": "/GameBoard/index.html"
+  },
+  {
+    "revision": "37c51312a5bffb7b770a",
+    "url": "/GameBoard/static/css/main.c17a7eb3.chunk.css"
+  },
+  {
+    "revision": "88b93aec53ae6b00f664",
+    "url": "/GameBoard/static/js/2.d532e1dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GameBoard/static/js/2.d532e1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c51312a5bffb7b770a",
+    "url": "/GameBoard/static/js/main.cc280fff.chunk.js"
+  },
+  {
+    "revision": "17c5d782f0cf8d286825",
+    "url": "/GameBoard/static/js/runtime-main.c5dbd50b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GameBoard/static/media/logo.5d5d9eef.svg"
+  }
+]);
