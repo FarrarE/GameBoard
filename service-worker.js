3261cec4aa235a0f7d98632773e814ab7d785054
Ezra <ezra.farrar@gmail.com>
2020-09-22 18:54:40 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GameBoard/precache-manifest.0d6abf8c1c4da7d3999759261cd20566.js"
+  "/GameBoard/precache-manifest.c1a0c6418d24ad6edd9e6bdcdf6f7c21.js"
 );
 
 self.addEventListener('message', (event) => {
