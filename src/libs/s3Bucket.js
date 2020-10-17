b53259ea7461ddbae9eecd8cee7b127016a1a99b
Ezra <ezra.farrar@gmail.com>
2020-10-17 02:55:16 -0700

    whitespace tinkering.

--- src/libs/s3Bucket.js
+++ src/libs/s3Bucket.js
@@ -1,7 +1,7 @@
 import { Storage } from "aws-amplify";
 
-export async function s3Upload(file) {
-  const filename = `${Date.now()}-${file.name}`;
+export async function s3Upload(file, type) {
+  const filename = `${Date.now()}${type}`;
 
   const stored = await Storage.vault.put(filename, file, {
     contentType: file.type,
