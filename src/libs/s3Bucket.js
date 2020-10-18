f3a8fd0b63c5bf4c2abe8212aac85561c84e42aa
Ezra <ezra.farrar@gmail.com>
2020-10-18 03:36:35 -0700

    Updated parameters for upload type: map / token

--- src/libs/s3Bucket.js
+++ src/libs/s3Bucket.js
@@ -1,7 +1,7 @@
 import { Storage } from "aws-amplify";
 
-async function s3Upload(file, type) {
-  const filename = `${Date.now()}${"map"}`;
+async function s3Upload(file, type, tag) {
+  const filename = `${Date.now()}${tag}`;
 
   const stored = await Storage.vault.put(filename, file, {
     contentType: type,
