b42a222638ecf7a1f896f67175db6c7a7b1e17dd
Ezra <ezra.farrar@gmail.com>
2020-10-18 19:05:01 -0700

    external api wrapper for remove

---
+++ src/libs/deleteFiles.js
@@ -0,0 +1,14 @@
+import { Storage } from "aws-amplify";
+import { API } from "aws-amplify";
+import updateFile from './updateFile';
+
+export default async function deleteFiles(id, boardState, key) {
+    const stored = {
+        db: null,
+        s3: null
+    }
+
+    stored.db = await updateFile(boardState, id);
+    stored.s3 = await Storage.vault.remove(key);
+    return stored;
+}
