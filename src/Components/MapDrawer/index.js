eb1e10b168204402ce02cc7b9fa4f5ee17a91616
Ezra <ezra.farrar@gmail.com>
2020-10-18 20:00:39 -0700

    Added selector highlight implementation.

--- src/Components/MapDrawer/index.js
+++ src/Components/MapDrawer/index.js
@@ -1,41 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BsPlusSquare } from 'react-icons/bs';
 import './index.css';
 import * as Constants from '../../constants';
 
 function MapDrawer(props) {
+  const [selected, setSelected] = useState(null);
+  const [className, setClassName] = useState("selected");
 
-  
+  useEffect(() => {
+  }, [selected]);
 
+  function setFocus(event) {
+    setSelected(parseInt(event.target.id[0]));
+  }
 
-    let drawerState = 'map-drawer';
-    if(props.state === "drawerOpen"){
-        drawerState = 'map-drawer open'
-    }
-    else {
-        drawerState = 'map-drawer';
-    }
+  let drawerState = 'map-drawer';
+  if (props.state === "drawerOpen") {
+    drawerState = 'map-drawer open'
+  }
+  else {
+    drawerState = 'map-drawer';
+  }
 
   return (
-    
-    <div className={drawerState} > 
+
+    <div className={drawerState} >
       <form action="/action_page.php" className="map-form">
         <label for="file-input" >
           <div className="add-icon">
-            <BsPlusSquare  />
+            <BsPlusSquare />
           </div>
         </label>
-        <input id="file-input" type="file" onChange={props.getMap}/>
+        <input id="file-input" type="file" onChange={props.getMap} />
       </form>
       <div className="map-container">
-        {props.maps[0] ?  
-          props.maps.map((map, index) =>(
+        {props.maps[0] ?
+          props.maps.map((map, index) => (
             <div className="map-thumbnails">
-              <img src={map.img.src} alt="..." id={index +"map"} width="100" height="50" onClick={props.changeMap}/>
+              {(selected === index) ?
+                <img
+                  className={className}
+                  src={map.img.src}
+                  alt="..."
+                  id={index + "m"}
+                  width="100"
+                  height="50"
+                  onDoubleClick={props.changeMap}
+                  onClick={setFocus}
+                />
+                :
+                <img
+                  src={map.img.src}
+                  alt="..."
+                  id={index + "map"}
+                  width="100"
+                  height="50"
+                  onDoubleClick={props.changeMap}
+                  onClick={setFocus}
+                />
+              }
+
               <img className="delete-map" src={Constants.DELETE_ICON} onClick={() => props.deleteMap(map.key)} />
             </div>
           ))
-         : 
+          :
           <p>Add a map. Switch maps by clicking on the thumbnails.</p>
         }
       </div>
