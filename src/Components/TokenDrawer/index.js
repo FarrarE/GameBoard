c32068928679f880cc523c971eb8df1e2ea672cc
Ezra <ezra.farrar@gmail.com>
2020-10-19 03:14:38 -0700

    normalized design to fit new format.

--- src/Components/TokenDrawer/index.js
+++ src/Components/TokenDrawer/index.js
@@ -13,7 +13,7 @@ function TokenDrawer(props) {
     setSelected(parseInt(event.target.id[0]));
   }
 
-  function hideOptions(event){
+  function hideOptions(event) {
     setSelected(null);
   }
 
@@ -27,52 +27,54 @@ function TokenDrawer(props) {
 
   return (
     <div className={drawerState} >
-      <form action="/action_page.php" className="token-form">
-        <label for="token-input" >
-          <div className="add-icon">
-            <BsPlusSquare />
-          </div>
-        </label>
-        <input id="token-input" type="file" onChange={props.getToken} />
-      </form>
       <div className="token-container">
-
-
-        {props.tokens[0] ? props.tokens.map((token, index) => (
-          <div className="token-img">
-            <Droppable id={index + "droppable"} >
-              <Draggable id={index + "token"} >
-                <div className="token-thumbnails">
-                  {(selected === index) ?
-                    <img
-                      className={className}
-                      alt="..."
-                      id={index + "token"}
-                      draggable="true" src={token.img.src}
-                      width="100" height="100"
-                      onDoubleClick={showOptions}
-                      onClick={hideOptions}
-                    />
-                    :
-                    <img
-                      alt="..."
-                      id={index + "token"}
-                      draggable="true" src={token.img.src}
-                      width="100" height="100"
-                      onDoubleClick={showOptions}
-                      onClick={hideOptions}
-                    />
-                  }
-                  <img className="delete-token" src={Constants.DELETE_ICON} onClick={() => props.deleteToken(token.key)} />
-                </div>
-              </Draggable>
-            </Droppable>
-          </div>
-        ))
-          :
-          <p>Add a token. Drag and drop the token onto the grid. Scale token to grid by double clicking the token.</p>
-        }
+        <div className="upload-form-token">
+          <form action="/action_page.php" className="token-form">
+            <label for="token-input" >
+              <div className="add-icon-token">
+                  <BsPlusSquare />
+              </div>
+            </label>
+            <input id="token-input" type="file" onChange={props.getToken} />
+          </form>
+        </div>
+        <div className="token-container">
+          {props.tokens[0] ? props.tokens.map((token, index) => (
+            <div className="token-img">
+              <Droppable id={index + "droppable"} >
+                <Draggable id={index + "token"} >
+                  <div className="token-thumbnails">
+                    {(selected === index) ?
+                      <img
+                        className={className + " " + "token"}
+                        alt="..."
+                        id={index + "token"}
+                        draggable="true" src={token.img.src}
+                        onDoubleClick={showOptions}
+                        onClick={hideOptions}
+                      />
+                      :
+                      <img
+                        className="token"
+                        alt="..."
+                        id={index + "token"}
+                        draggable="true" src={token.img.src}
+                        onDoubleClick={showOptions}
+                        onClick={hideOptions}
+                      />
+                    }
+                    <img className="delete-token" src={Constants.DELETE_ICON} onClick={() => props.deleteToken(token.key)} />
+                  </div>
+                </Draggable>
+              </Droppable>
+            </div>
+          ))
+            :
+            <p>Add a token. Drag and drop the token onto the grid. Scale token to grid by double clicking the token.</p>
+          }
+        </div>
       </div>
+
     </div>
   );
 }
