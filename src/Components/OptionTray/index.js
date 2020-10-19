d8075ff1f31ddfb210c494550778db23d2d1c1a5
Ezra <ezra.farrar@gmail.com>
2020-10-19 04:36:47 -0700

    Added drag icon and made draggable

--- src/Components/OptionTray/index.js
+++ src/Components/OptionTray/index.js
@@ -1,28 +1,40 @@
-import React from "react";
-import { RiLogoutBoxRLine } from 'react-icons/ri';
+import React, { useState, useEffect, } from "react";
+import Draggable from 'react-draggable';
+import { MdDragHandle } from 'react-icons/md';
 
 import './index.css';
 
 function OptionTray(props) {
+    const [element, setElement] = useState(null);
+    const [pos1, setPos1] = useState(0);
+    const [pos2, setPos2] = useState(0);
+    const [pos3, setPos3] = useState(0);
+    const [pos4, setPos4] = useState(0);
+    const [update, setUpdate] = useState(null);
 
     return (
-        <div className="options">
-            <div>
-                Grid Size
+        <Draggable>
+            <div className="options">
+                <div>
+                    <MdDragHandle className="drag-icon" />
+                </div>
+                <div>
+                    Grid Size
             </div>
-            <div>
-                <input type="range" min="10" max="100" class="slider" onInput={props.scaleGrid.bind(this)} />
+                <div>
+                    <input type="range" min="10" max="100" class="slider" onInput={props.scaleGrid.bind(this)} />
+                </div>
+                <div>
+                    Map Size
             </div>
-            <div>
-                Map Size
+                <div>
+                    <input type="range" min="10" max="100" class="slider" onInput={props.scaleMap.bind(this)} />
+                </div>
+                <div>
+                    <div className="logout-button" onClick={props.handleLogout.bind(this)}><span>Log Out</span></div>
+                </div>
             </div>
-            <div>
-                <input type="range" min="10" max="100" class="slider" onInput={props.scaleMap.bind(this)} />
-            </div>
-            <div>
-                <div className="logout-button" onClick={props.handleLogout.bind(this)}><span>Log Out</span></div>
-            </div>
-        </div>
+        </Draggable>
     );
 }
 
