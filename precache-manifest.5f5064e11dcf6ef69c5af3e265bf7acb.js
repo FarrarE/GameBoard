e606fe4f149619372fafccdf02f5b92d3a1f064b
Ezra <ezra.farrar@gmail.com>
2020-10-19 21:23:43 -0700

    Updates

--- precache-manifest.5f5064e11dcf6ef69c5af3e265bf7acb.js
+++ precache-manifest.5f5064e11dcf6ef69c5af3e265bf7acb.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42b3a6cdc235a989a0eb8e2d0e47360d",
+    "revision": "20e7fd297e5d591999101c60c3eb2f35",
     "url": "/GameBoard/index.html"
   },
   {
-    "revision": "94a2c0b87759440d66f2",
-    "url": "/GameBoard/static/css/main.5ce1e7f3.chunk.css"
+    "revision": "e3d2414f11ac941eb2e3",
+    "url": "/GameBoard/static/css/main.e538f052.chunk.css"
   },
   {
     "revision": "c15f1120eb2ebe8911c8",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/GameBoard/static/js/2.e2b8658b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "94a2c0b87759440d66f2",
+    "revision": "e3d2414f11ac941eb2e3",
     "url": "/GameBoard/static/js/main.79bd895e.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GameBoard/precache-manifest.c0a2e2508290ca7dff6351fb6c69feea.js"
+  "/GameBoard/precache-manifest.5f5064e11dcf6ef69c5af3e265bf7acb.js"
 );
 
 self.addEventListener('message', (event) => {
