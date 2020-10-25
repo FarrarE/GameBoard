80b8eb64a4518215565144ed1e73ca31c080464e
Ezra <ezra.farrar@gmail.com>
2020-10-24 21:28:54 -0700

    Refactor to include mode

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import './styles/index.css';
-import lightMode from "./styles/lightmode";
-import darkMode from "./styles/darkmode";
+import './styles/lightmode.css';
+import './styles/darkmode.css';
 
 function OptionTray(props) {
   const [email, setEmail] = useState(undefined);
   const [password, setPassword] = useState(undefined);
-  const [mode, setMode] = useState(darkMode);
+  const [mode, setMode] = useState("dark-mode");
 
   function validateForm() {
     if (email && password)
@@ -14,14 +14,14 @@ function OptionTray(props) {
   }
 
   return (
-    <div className="login-page" style={mode.loginPage}>
+    <div id="login-page" className={mode}>
       <link rel="stylesheet" type="text/css" href="./styles/lightmode.css" />
-      <div className="ribbon" style={mode.ribbon}>
-        <div className="title">
-          <h1 style={mode.h1}>Battlemaps</h1>
-          <p style={mode.subTitle}>Virtual Tabletop Toolset</p>
+      <div className={"ribbon ribbon-" + mode}>
+        <div className={"title title-" + mode}>
+          <h1 className={"h1-" + mode}>Battlemaps</h1>
+          <p>Virtual Tabletop Toolset</p>
         </div>
-        <div className="login-form" style={mode.loginForm}>
+        <div className="login-form">
           <form
             onSubmit={(e) => {
               props.handleSubmit(email, password);
@@ -40,25 +40,24 @@ function OptionTray(props) {
               onChange={e => setPassword(e.target.value)}
               type="password"
             />
-            <button className="login-button lightMode" disabled={!validateForm()} type="submit" tabIndex="3">
-              <span>
-                Login
+            <div>
+              <button className="login-button lightMode" disabled={!validateForm()} type="submit" tabIndex="3">
+                <span>
+                  Login
               </span>
-            </button>
-          </form>
-            <div className="links">
-              <span>Dont have an account?</span>
-              <a onClick={props.signUp} tabIndex="1"><span> Sign up here</span></a>
-              <span>or</span>
-              <a onClick={props.runTest} tabIndex="2"><span>Try without signing up</span></a>
+              </button>
             </div>
+
+          </form>
+          <div className="links">
+            <span>Dont have an account?</span>
+            <a onClick={props.signUp} tabIndex="1"><span> Sign up here</span></a>
+            <span>or</span>
+            <a onClick={props.runTest} tabIndex="2"><span>Try without signing up</span></a>
+          </div>
         </div>
-        </div>
-        <label class="switch">
-            <input type="checkbox" />
-              <span class="slider round"></span>
-          </label>
       </div>
+    </div>
   );
 }
 
