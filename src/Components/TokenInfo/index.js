74948c27ee098fcb512ed97d4e2d07de267853de
Ezra <ezra.farrar@gmail.com>
2020-11-19 19:03:54 -0800

    Added useEffect hook to grab prop information and populate state.

--- src/Components/TokenInfo/index.js
+++ src/Components/TokenInfo/index.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Draggable from 'react-draggable';
+import { MdDragHandle } from 'react-icons/md';
 import './index.css';
+import './styles/mode.css'
 
 function TokenInfo(props) {
-
-  return (
-    <div className="token-information">
-    </div>
-  );
+    const [display, setDisplay] = useState("none");
+    const [name, setName] = useState("");
+    useEffect(() =>{
+        if(props.display){
+            setDisplay(props.display);
+            setName(props.display);
+        }
+            
+    },[]);
+    
+    return (
+        <div className={props.mode + " " + display}>
+            <Draggable handle=".handle">
+                <div className="token-information">
+                    <div>
+                        <MdDragHandle className="drag-icon handle" />
+                    </div>
+                    <input type="text" placeholder="Name" value={name}/>
+                    <button>Save</button>
+                </div>
+            </Draggable>
+        </div>
+    );
 }
 
 export default TokenInfo;
