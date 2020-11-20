cd538a19de6868a69bd5a1e694de5782e36a969d
Ezra <ezra.farrar@gmail.com>
2020-11-19 16:48:57 -0800

    Added a search bar input and changed the containerization of token elements. Search not implemented. Token wrap bug fixed.

--- src/Components/TokenDrawer/index.js
+++ src/Components/TokenDrawer/index.js
@@ -28,7 +28,10 @@ function TokenDrawer(props) {
 
   return (
     <div className={props.mode}>
-      <div className={drawerState}> 
+      <div className={drawerState}>
+        <div className="searchbar">
+          <input type="text" placeholder="Search.."></input>
+        </div>
         <div className="token-container">
           <div className="upload-form-token">
             <form action="/action_page.php" className="token-form">
@@ -40,41 +43,39 @@ function TokenDrawer(props) {
               <input id="token-input" type="file" onChange={props.getToken} />
             </form>
           </div>
-          <div className="token-container">
-            {props.tokens[0] ? props.tokens.map((token, index) => (
-              <div className="token-img">
-                <Droppable id={index + "droppable"} >
-                  <Draggable id={index + "token"} >
-                    <div className="token-thumbnails">
-                      {(selected === index) ?
-                        <img
-                          className={className + " " + "token"}
-                          alt="..."
-                          id={index + "token"}
-                          draggable="true" src={token.img.src}
-                          onDoubleClick={showOptions}
-                          onClick={hideOptions}
-                        />
-                        :
-                        <img
-                          className="token"
-                          alt="..."
-                          id={index + "token"}
-                          draggable="true" src={token.img.src}
-                          onDoubleClick={showOptions}
-                          onClick={hideOptions}
-                        />
-                      }
-                      <img className="delete-token" src={Constants.DELETE_ICON} onClick={() => props.deleteToken(token.key)} />
-                    </div>
-                  </Draggable>
-                </Droppable>
-              </div>
-            ))
-              :
-              <p>Add a token. Drag and drop the token onto the grid. Scale token to grid by double clicking the token.</p>
-            }
-          </div>
+          {props.tokens[0] ? props.tokens.map((token, index) => (
+            <div className="token-img">
+              <Droppable id={index + "droppable"} >
+                <Draggable id={index + "token"} >
+                  <div className="token-thumbnails">
+                    {(selected === index) ?
+                      <img
+                        className={className + " " + "token"}
+                        alt="..."
+                        id={index + "token"}
+                        draggable="true" src={token.img.src}
+                        onDoubleClick={showOptions}
+                        onClick={hideOptions}
+                      />
+                      :
+                      <img
+                        className="token"
+                        alt="..."
+                        id={index + "token"}
+                        draggable="true" src={token.img.src}
+                        onDoubleClick={showOptions}
+                        onClick={hideOptions}
+                      />
+                    }
+                    <img className="delete-token" src={Constants.DELETE_ICON} onClick={() => props.deleteToken(token.key)} />
+                  </div>
+                </Draggable>
+              </Droppable>
+            </div>
+          ))
+            :
+            <p>Add a token. Drag and drop the token onto the grid. Scale token to grid by double clicking the token.</p>
+          }
         </div>
       </div>
     </div>
