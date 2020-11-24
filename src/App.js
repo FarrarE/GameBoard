d196ef23c2d71f790497109450c0d130a78b654c
Ezra <ezra.farrar@gmail.com>
2020-11-23 21:45:43 -0800

    Added base mode functionality

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React, { useState, useEffect, useRef } from "react";
 
 import LoginPage from './Containers/LoginPage';
 
-
-
 // css and constants
 import './App.css';
-import * as Constants from './constants';
 import testToken1 from './Data/tokens/dax.jpg';
 import testToken2 from './Data/tokens/pop.jpg';
 
 function App(props) {
-  const [mode, setMode] = useState(null);
+  const [mode, setMode] = useState(defaultMode(localStorage.getItem('mode')));
   useEffect(() => {
   }, []);
 
@@ -21,13 +18,22 @@ function App(props) {
     else return stored;
   }
 
+  function toggleMode() {
+    if (mode === "light-mode") {
+      setMode("dark-mode")
+      localStorage.setItem('mode', 'dark-mode');
+    } else {
+      setMode("light-mode")
+      localStorage.setItem('mode', 'light-mode');
+    }
+  }
 
   return (
     <div className="App">
       <LoginPage
         mode={mode}
+        toggleMode={toggleMode}
       />
-
     </div>
   );
 }
