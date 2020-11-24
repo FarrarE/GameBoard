78724270161a1aeb57ef776feb58cd66c28bae3f
Ezra <ezra.farrar@gmail.com>
2020-11-23 20:11:23 -0800

    Added containers to encapuslate functionality.

---
+++ src/Containers/LoginPage.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+function LoginPage(props) {
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div >
+
+    </div>
+  );
+}
+
+export default LoginPage;
